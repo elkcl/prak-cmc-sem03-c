0a5a4a1dfeb11c03ecb870af1df060e19d83408b
Денисьев Илья <s02220082@study-00.ru-central1.internal>
2023-09-20 18:42:58 +0000

    mz03-4 fixed

--- mz03/mz03-4.c
+++ mz03/mz03-4.c
@@ -4,7 +4,7 @@ satsum(int v1, int v2)
     enum
     {
         MY_INT_MAX = (unsigned) ~0 >> -~0,
-        MY_INT_MIN = -MY_INT_MAX - 1
+        MY_INT_MIN = -MY_INT_MAX + ~0
     };
     if (v1 > 0 && v2 > 0 && MY_INT_MAX - v1 <= v2) {
         return MY_INT_MAX;
