cc999c80f0ec3aca83d02e8109a51da094b17467
Денисьев Илья <s02220082@study-00.ru-central1.internal>
2023-09-20 11:32:57 +0000

    mz03-4 simplified

--- mz03/mz03-4.c
+++ mz03/mz03-4.c
@@ -3,11 +3,8 @@ satsum(int v1, int v2)
 {
     enum
     {
-        MINUS_ONE = ~0,
-        ONE = -MINUS_ONE,
-        THIRTY_TWO = ONE << (ONE + ONE + ONE + ONE + ONE),
-        MY_INT_MIN = (int) ((unsigned int) ONE << (THIRTY_TWO - ONE)),
-        MY_INT_MAX = -(MY_INT_MIN + ONE)
+        MY_INT_MAX = (unsigned) ~0 >> -~0,
+        MY_INT_MIN = -MY_INT_MAX - 1
     };
     if (v1 > 0 && v2 > 0 && MY_INT_MAX - v1 <= v2) {
         return MY_INT_MAX;
