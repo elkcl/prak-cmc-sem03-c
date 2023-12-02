0e00e56d1a16f7f96f2368222dc8df8877ecc719
Денисьев Илья <s02220082@study-00.ru-central1.internal>
2023-12-02 19:19:34 +0000

    mz13-5 almost

--- mz13/mz13-5.c
+++ mz13/mz13-5.c
@@ -1,10 +1,7 @@
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif /* ifndef _GNU_SOURCE */
-
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -28,31 +25,50 @@ panic(char *err)
     _exit(1);
 }
 
-void
+pid_t
 ping_pong_player(int id, int lim, int fd_out[static 2], int fd_in[static 2])
 {
-    if (close(fd_out[0]) == -1) {
-        panic("close failed");
-    }
-    if (close(fd_in[1]) == -1) {
-        panic("close failed");
-    }
-    int x;
-    while (dscanf(fd_in[0], "%d", &x) == 1) {
-        printf("%d %d\n", id, x);
-        ++x;
-        if (x >= lim) {
-            break;
+    pid_t pid = fork();
+    if (pid < 0) {
+        panic("fork failed");
+    } else if (pid == 0) {
+        if (close(fd_out[0]) == -1) {
+            panic("close failed");
         }
-        dprintf(fd_out[1], "%d\n", x);
+        if (close(fd_in[1]) == -1) {
+            panic("close failed");
+        }
+        FILE *in = fdopen(fd_in[0], "r");
+        if (in == NULL) {
+            panic("fdopen failed");
+        }
+        setbuf(in, NULL);
+        FILE *out = fdopen(fd_out[1], "w");
+        if (out == NULL) {
+            panic("fdopen failed");
+        }
+        setbuf(out, NULL);
+        int x;
+        while (fscanf(in, "%d", &x) == 1) {
+            if (x >= lim) {
+                break;
+            }
+            printf("%d %d\n", id, x);
+            ++x;
+            if (x >= lim) {
+                break;
+            }
+            fprintf(out, "%d\n", x);
+        }
+        if (fclose(in) != 0) {
+            panic("close failed");
+        }
+        if (fclose(out) != 0) {
+            panic("close failed");
+        }
+        _exit(0);
     }
-    if (close(fd_out[1]) == -1) {
-        panic("close failed");
-    }
-    if (close(fd_in[0]) == -1) {
-        panic("close failed");
-    }
-
+    return pid;
 }
 
 int
@@ -71,10 +87,22 @@ main(int argc, char *argv[])
     if (pipe(fd1) == -1 || pipe(fd2) == -1) {
         panic("pipe failed");
     }
-    pid_t pid1 = fork();
-    if (pid1 < 0) {
-        panic("fork failed");
-    } else if (pid1 == 0) {
+    ping_pong_player(1, lim, fd1, fd2);
+    ping_pong_player(2, lim, fd2, fd1);
+    dprintf(fd2[1], "%d\n", 1);
+    if (close(fd1[0]) == -1) {
+        panic("close failed");
     }
+    if (close(fd1[1]) == -1) {
+        panic("close failed");
+    }
+    if (close(fd2[0]) == -1) {
+        panic("close failed");
+    }
+    if (close(fd2[1]) == -1) {
+        panic("close failed");
+    }
+    while (wait(NULL) > 0) {
+    }
+    puts("Done");
 }
-
